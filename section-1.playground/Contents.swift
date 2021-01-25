b0f0ed2e91159681229128a0d5ee5fbe9cf4b5a5
nlhomme <9362475+nlhomme@users.noreply.github.com>
2021-01-25 19:55:56 +0100

    Section1

---
+++ section-1.playground/Contents.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+// Challenge 1
+let myAge: Int = 28
+var dogs: Int = 0
+dogs += 1
+
+
+// Challenge 2
+var age: Int = 16
+print(age)
+age = 30
+print(age)
+
+
+// Challenge 3
+let x: Int = 46
+let y: Int = 10
+
+// 1
+let answer1: Int = (x * 100) + y
+// Vaut 4610
+
+// 2
+let answer2: Int = (x * 100) + (y * 100)
+// Vaut 5600
+
+// 3
+let answer3: Int = (x * 100) + (y / 10)
+// Vaut 4601
+
+
+// Challenge 4
+8 - 4 * 2 + 6 / 3 * 4
+// Vaut 8
+
+8 - (4 * 2) + (6 / 3) * 4
+// Vaut 8
+
+
+// Challenge 5
+let rating1: Double = 4
+let rating2: Double = 7
+let rating3: Double = 9
+
+let averageRating = (rating1 + rating2 + rating3) / 3
+
+
+// Challenge 6
+let voltage: Double = 12
+let current: Double = 3.5
+let power: Double = voltage * current
+
+
+// Challenge 7
+let resistance: Double = power / (current * current)
+//let resistance: Double = power / pow(current, 2)
+
+
+// Challenge 8
+let randomNumber = arc4random()
+let diceRoll = (randomNumber % 6 ) + 1
+
+
+// Challenge 9
+//let a: Double
+//let b: Double
+//let c: Double
