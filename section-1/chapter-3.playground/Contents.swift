8ca9ae17e2b9f6f7f9c3bc6132ec0b3829723943
nlhomme <9362475+nlhomme@users.noreply.github.com>
2021-01-31 20:04:57 +0100

    Bool: mini-exercises

---
+++ section-1/chapter-3.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// Bool: mini-exercises
+// 1
+let myAge = 28
+let isTeenager = myAge <= 13 && myAge >= 19
+
+// 2
+let theirAge = 30
+let bothTeenager = isTeenager && (theirAge <= 13 && theirAge >= 19)
+
+// 3
+let reader = "Nicolas Lhomme"
+let author = "Matt Galloway"
+let authorIsReader = reader == author
+
+// 4
+let readerBeforeAuthor = reader < author
+
+
