7a75856a9421448920c7a4a458b7457691e9acb7
nlhomme <9362475+nlhomme@users.noreply.github.com>
2021-02-05 19:43:34 +0100

    chapter 4: For switch statements: mini-exercises

--- section-1/chapter-4.playground/Contents.swift
+++ section-1/chapter-4.playground/Contents.swift
@@ -22,3 +22,42 @@ for row in 0..<8 where (row % 2 != 0) {
     print(sum)
 }
 
+
+// Switch statements: mini-exercises
+// 1
+let age = Int.random(in: 0...100)
+print(age)
+
+switch age {
+case 0...2:
+    print("Infant")
+case 3...12:
+    print("Child")
+case 13...19:
+    print("Teenager")
+case 20...39:
+    print("Adult")
+case 40...60:
+    print("Middle-aged")
+default:
+    print("Elderly")
+}
+
+// 2
+let tuple = (name:"nicolas", age:Int.random(in: 0...100))
+print(tuple.age)
+
+switch tuple {
+case (_, 0...2):
+    print(tuple.name, "is a infant")
+case (_, 3...12):
+    print(tuple.name, "is a child")
+case (_, 13...19):
+    print(tuple.name, "is a teenager")
+case (_, 20...39):
+    print(tuple.name, "is an adult")
+case (_, 40...60):
+    print(tuple.name, "is a middle-aged")
+default:
+    print(tuple.name, "is an ederly")
+}
