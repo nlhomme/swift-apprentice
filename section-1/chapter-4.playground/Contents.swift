a88b9c8aafc8415ca43d385483f5747b8dd4877c
nlhomme <9362475+nlhomme@users.noreply.github.com>
2021-02-05 20:12:26 +0100

    chapter 4: Challenges

--- section-1/chapter-4.playground/Contents.swift
+++ section-1/chapter-4.playground/Contents.swift
@@ -61,3 +61,59 @@ case (_, 40...60):
 default:
     print(tuple.name, "is an ederly")
 }
+
+
+// Challenges
+// Challenge 1: How many times
+/*
+var sum = 0
+ for i in 0...5 {
+   sum += i
+ }
+*/
+// The iteration will happen 6 times.
+
+// Challenge 2: Count the letter
+/*
+var aLotOfAs = ""
+while aLotOfAs.count < 10 {
+ aLotOfAs += "a"
+}
+*/
+// aLotOfAs contains 10 "a"
+
+// Challenge 3: What will print
+/*
+switch coordinates {
+case let (x, y, z) where x == y && y == z:
+ print("x = y = z")
+case (_, _, 0):
+ print("On the x/y plane")
+case (_, 0, _):
+ print("On the x/z plane")
+case (0, _, _):
+ print("On the y/z plane")
+default:
+ print("Nothing special")
+}
+*/
+
+//let coordinates = (1, 5, 0) // prints "On the x/y plane"
+//let coordinates = (2, 2, 2) // prints "x = y = z"
+//let coordinates = (3, 0, 1) // prints "On the x/z plane"
+//let coordinates = (3, 2, 5) // prints "Nothing special"
+//let coordinates = (0, 2, 4) // prints "On the y/z planee"
+
+// Challenge 4: Closed range size
+// A closed range cannot be empty because it should onclude a start and an end value
+
+// Challenge 5: The final countdown
+for i in 0...10 {
+    print(10 - i)
+}
+
+// Challenge 6: Print a sequence
+for i in 0...10 {
+    print(Double(i) / 10)
+}
+
