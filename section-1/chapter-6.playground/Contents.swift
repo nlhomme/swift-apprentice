1e355b67ab292e30f7befa4780be1e98d814e3c9
nlhomme <9362475+nlhomme@users.noreply.github.com>
2021-02-08 19:57:44 +0100

    chapter 6: Introducing optionals: mini-exercises

---
+++ section-1/chapter-6.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+// Introducing optionals: mini-exercises
+// 1
+var myFavoriteSong: String? = "Careless Whispers"
+
+// 2
+let parsedInt = Int("10")
+// parsedInt is an optional if the parsed String is "" or not a String, parsedInt equals nil
+
+// 3
+let parsedInt3 = Int("cat") // equals nil
