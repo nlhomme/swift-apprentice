ec75218de41fa5e202b2dca1ba9e4057643033c2
Nicolas Lhomme <9362475+nlhomme@users.noreply.github.com>
2021-02-19 15:00:04 +0100

    chapter 7: iterating through arrays: mini-exercice

--- section-2/chapter-7.playground/Contents.swift
+++ section-2/chapter-7.playground/Contents.swift
@@ -1,7 +1,13 @@
 import UIKit
 
-// Modifying arrays: mini exercise
+// Modifying arrays: mini-exercise
 var players = ["Alice", "Bob", "Cindy", "Dan", "Eli"]
 if let danPosition = players.firstIndex(of: "Dan") {
     print("Dan is at position \(danPosition)")
+}
+
+// Iterating through an array: mini-exercise
+var scores = [2, 2, 8, 6, 7]
+for (index, player) in players.enumerated() {
+    print("\(player) has scored \(scores[index]) point(s)")
 }
