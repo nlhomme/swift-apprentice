e86713a8b53a0602b28466ae7996554bf7f8d125
Nicolas Lhomme <9362475+nlhomme@users.noreply.github.com>
2021-02-21 14:16:39 +0100

    Modifying dictionaries: mini-exercise

--- section-2/chapter-7.playground/Contents.swift
+++ section-2/chapter-7.playground/Contents.swift
@@ -11,3 +11,22 @@ var scores = [2, 2, 8, 6, 7]
 for (index, player) in players.enumerated() {
     print("\(player) has scored \(scores[index]) point(s)")
 }
+
+
+// Modifying dictionaries: mini-exercise
+var bobData = [
+    "name": "Bob",
+    "profession": "Card player",
+    "country": "USA",
+    "state": "CA",
+    "city": "San Francisco"
+]
+
+func playerLocation (for player: [String:String]) {
+    if let city = player["city"], let state = player["state"] {
+            print(city, "-", state)
+        
+    }
+}
+playerLocation(for: bobData)
+
