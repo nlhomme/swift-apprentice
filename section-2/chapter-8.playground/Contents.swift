c9a27a0580ef572aa610aae2a354677ade36c63e
Nicolas Lhomme <9362475+nlhomme@users.noreply.github.com>
2021-03-15 19:48:49 +0100

    Iterations with closures: mini-exercises

---
+++ section-2/chapter-8.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+// Iteration with closures: mini-exercises
+// 1
+let names = ["Jacques", "Lionel", "Edouard", "Jean-Pierre", "Dominique", "Eric"]
+let allNames = names.reduce("") {
+    $0 + $1
+}
+// print(allNames)
+
+// 2
+let allLongNames = names.filter{$0.count > 4}.reduce("") {
+    $0 + $1
+}
+// print(allLongNames)
+
+// 3
+let namesAndAges = ["Jacques": 42, "Lionel": 53, "Edouard": 39, "Jean-Pierre": 17, "Dominique": 72, "Eric": 2]
+
+let underageOnly = namesAndAges.filter {
+    $0.value < 18
+}
+// print(underageOnly)
+
+// 4
+let adultsOnlyNames = namesAndAges.filter {$0.value >= 18}.map {
+    $0.key
+}
+//print(adultsOnlyNames)
