e4741bcda8f4cbf73d48c3f32075bf94686fb504
Nicolas Lhomme <9362475+nlhomme@users.noreply.github.com>
2021-04-09 16:58:12 +0200

    chapter 8: challenges completed

--- section-2/chapter-8.swift
+++ section-2/chapter-8.swift
@@ -41,3 +41,37 @@ repeatTask(times: 10) {
     print("Swift apprentice is a great book!")
 }
 
+// Challenge 2: Closures sums
+// It took the solution as is, I unsterstood nothing on this challenge... :(
+func mathSum(length: Int, series: (Int) -> Int) -> Int {
+    var result = 0
+    for i in 1...length {
+        result += series(i)
+    }
+    return result
+}
+
+mathSum(length: 10) { number in
+    number * number
+}
+
+// Challenge 3: Functionnal rating
+let appRatings = [
+    "Calendar Pro": [1, 5, 5, 4, 2, 1, 5, 4],
+    "The Messenger": [5, 4, 2, 5, 4, 1, 1, 2],
+    "Socialise": [2, 1, 2, 2, 1, 2, 4, 2]
+]
+
+var averageRatings: [String:Int] = [:]
+appRatings.forEach {
+    let total = $0.value.reduce(0, +)
+    averageRatings[$0.key] = (total / $0.value.count)
+}
+//print(averageRatings)
+
+let goodApps = averageRatings.filter {
+    $0.value > 3
+}.map{
+    $0.key
+}
+//print(goodApps)
